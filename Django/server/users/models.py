f13a643e4fe454d961fe63a5592bdc1f2a6cfa8a
Iv <ivcho2213@gmail.com>
2020-10-21 17:07:52 +0200

    Email for the user is now unique in DB

--- Django/server/users/models.py
+++ Django/server/users/models.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20,null=True)
     
+    class Meta(object):
+        unique_together = ('email',)
 
     def __str__(self):
         return self.username
