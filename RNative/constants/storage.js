0bb4d584510639a5f7c8118dd2d79424283cbef6
Iv <ivcho2213@gmail.com>
2020-11-14 21:40:00 +0100

    Filter screen

--- RNative/constants/storage.js
+++ RNative/constants/storage.js
@@ -5,4 +5,4 @@ export const user = makeVar([]);
 export const favouriteHouses = makeVar({})
 
 export const allHouses = makeVar({})
-export const searchedData = makeVar([])
+export const searchedData = makeVar("empty")
